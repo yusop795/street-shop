4fab322a75dcb8d9620013c2d6bb2f0b62565036
haein <kendy93@sbcn.co.kr>
2019-10-12 13:23:20 +0900

    Bottm Nav 작업
    - Bottm Nav 컴포넌트 작성
    - common theme 작성

--- src/components/Nav/BottomNav.jsx
+++ src/components/Nav/BottomNav.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Icon } from 'antd';
 import './nav.scss';
 
 export default function BottomNav() {
   const activeStyle = {
-    color: 'white',
+    opacity: 1,
   };
+
   return (
     <div id="bottomNav">
-      <div id="home"><NavLink exact to="/" activeStyle={activeStyle}>홈</NavLink></div>
-      <div id="rank"><NavLink exact to="/shop" activeStyle={activeStyle}>랭킹</NavLink></div>
-      <div id="favorite"><NavLink exact to="/favorite" activeStyle={activeStyle}>좋아</NavLink></div>
-      <div id="myPage"><NavLink exact to="/myPage" activeStyle={activeStyle}>마이</NavLink></div>
+      <div id="home">
+        <NavLink exact to="/" activeStyle={activeStyle}>
+          <Icon type="home" />
+        </NavLink>
+      </div>
+      <div id="rank">
+        <NavLink exact to="/shop" activeStyle={activeStyle}>
+          <Icon type="rise" />
+        </NavLink>
+      </div>
+      <div id="favorite">
+        <NavLink exact to="/favorite" activeStyle={activeStyle}>
+          <Icon type="heart" />
+        </NavLink>
+      </div>
+      <div id="myPage">
+        <NavLink exact to="/myPage" activeStyle={activeStyle}>
+          <Icon type="user" />
+        </NavLink>
+      </div>
     </div>
   );
 }
