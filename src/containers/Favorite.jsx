31f921756ecaa7217d4c10092ed939250b034a87
yusop <yusop795@gmail.com>
2019-11-23 14:38:08 +0900

    새로운 리듀서에 맞춰서 컴포넌트 렌더

--- src/containers/Favorite.jsx
+++ src/containers/Favorite.jsx
@@ -1,23 +1,23 @@
 /* eslint-disable react/no-array-index-key */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { HeaderNav, BottomNav } from '../components/Nav';
-import { ShopList } from '../components/List';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { HeaderNav, BottomNav } from "../components/Nav";
+import { ShopList } from "../components/List";
+import { shopAction } from "../reducers/shopReducer";
 
 class Favorite extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      shop: [],
+      shop: this.props.shop
     };
   }
-
   render() {
     return (
-      <div className='container'>
-        <HeaderNav left={'<'} title='FAVORITE LIST' right={false} />
-        <div className='content_wrap'>
-          <ul className='favorite_list'>
+      <div className="container">
+        <HeaderNav left={"<"} title="FAVORITE LIST" right={false} />
+        <div className="content_wrap">
+          <ul className="favorite_list">
             {this.state.shop.map((data, i) => {
               return <ShopList key={i} shopInfo={data} />;
             })}
@@ -29,8 +29,18 @@ class Favorite extends Component {
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ shopReducer }) => ({
+  shop: shopReducer.shop
+});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  fetchShop: () => dispatch(shopAction.fetchShop()),
+  updateShop: shopInfo => dispatch(shopAction.updateShop(shopInfo)),
+  createShop: () => dispatch(shopAction.createShop()),
+  deleteShop: id => dispatch(shopAction.deleteShop(id))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Favorite);
--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { HeaderNav, BottomNav } from '../components/Nav';
-import { ShopList } from '../components/List';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { HeaderNav, BottomNav } from "../components/Nav";
+import { ShopList } from "../components/List";
+import { shopAction } from "../reducers/shopReducer";
 
 class Shop extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // shop: this.props.shop,
+      shop: this.props.shop
     };
   }
 
   render() {
     return (
-      <div className='container'>
-        <HeaderNav left={'<'} title='BEST SHOP' right={false} />
-        <div className='content_wrap'>
-          <ul className='ranking_list'>
-            {/* {this.state.shop.map((data, i) => {
+      <div className="container">
+        <HeaderNav left={"<"} title="BEST SHOP" right={false} />
+        <div className="content_wrap">
+          <ul className="ranking_list">
+            {this.state.shop.map((data, i) => {
               return <ShopList key={i} shopInfo={data} rank index={i + 1} />;
-            })} */}
+            })}
           </ul>
         </div>
         <BottomNav />
@@ -28,8 +29,18 @@ class Shop extends Component {
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ shopReducer }) => ({
+  shop: shopReducer.shop
+});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  fetchShop: () => dispatch(shopAction.fetchShop()),
+  updateShop: shopInfo => dispatch(shopAction.updateShop(shopInfo)),
+  createShop: () => dispatch(shopAction.createShop()),
+  deleteShop: id => dispatch(shopAction.deleteShop(id))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Shop);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Shop);
